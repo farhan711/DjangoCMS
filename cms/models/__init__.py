9725ee068d3b5c1cb217911adf029e59ba898421
farhan7011 <farhan7015@gmail.com>
2016-11-04 14:24:48 +0530

    Models added Apphooks and alias

---
+++ cms/models/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from .settingmodels import *  # nopyflakes
+from .pagemodel import *  # nopyflakes
+from .permissionmodels import *  # nopyflakes
+from .placeholdermodel import *  # nopyflakes
+from .pluginmodel import *  # nopyflakes
+from .titlemodels import *  # nopyflakes
+from .placeholderpluginmodel import *  # nopyflakes
+from .static_placeholder import *  # nopyflakes
+from .aliaspluginmodel import *  # nopyflakes
+from .apphooks_reload import *  # nopyflakes
+# must be last
+from cms import signals as s_import  # nopyflakes
